bde37e1ab2aff82ae6f3864ca363b40dda57dad1
karim gomez <karim.gomez29@yahoo.com>
2021-10-12 09:36:14 -0400

    finished category Routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,70 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['category_name', 'id'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'category_id', 'stock']
+    }]
+  }).then((category) => {
+    res.json(category)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findByPk({
+    attributes: ['category_name', 'id'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'category_id', 'stock']
+    }]
+  }).then((categories) => {
+    res.json(categories)
+  })
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    let categoryData = await Category.create({
+      category_name: req.body.category_name
+    });
+    res.status(200), JSON(categoryData)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    let categoryUpdate = await Category.update({
+      category_name: req.body.category_name
+    },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200), JSON(categoryUpdate)
+  } catch (error) {
+    res.status(400).json(error)
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then((categoryDelete) => {
+    res.json(categoryDelete);
+  }).catch((error) => res.json(error))
 });
 
 module.exports = router;
